4930fb0d3d40ce6b4e8dc94947da0d11ecd0d995
Chris <ChristopherAllenHunt@gmail.com>
2019-03-03 15:53:43 -0800

    Simple driver test.

--- src/app/driver/driver.cc
+++ src/app/driver/driver.cc
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#include "gpio/gpio.h"
+
 int main(int argc, char **argv) {
-    printf("Here!\n");
-    return 0;
+  // Create GPIO object
+  GPIO gpio(4);
+
+  // Set pin high
+  fprintf(stderr, "Setting pin hi!\n");
+  gpio.exportPin();
+  gpio.setDirection("out");
+  gpio.setValue(1);
+  fprintf(stderr, "Done!\n");
+  return 0;
 }
